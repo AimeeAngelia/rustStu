a1e847b4e6eb6de9cccf0e20b57ec42350527253
53coder <AimeeAngelia2022@Gmail.com>
2025-01-18 15:13:35 +0800

    7.1,7.2和7.3
    `包` `Crate` `作用域` `私有性` `模块路径`

---
+++ src/restaurant/target/debug/deps/restaurant-5525da1842a10883.d
@@ -0,0 +1,7 @@
+C:\Users\Aimee\Documents\GitHub\rustStu\src\restaurant\target\debug\deps\librestaurant-5525da1842a10883.rmeta: src\lib.rs
+
+C:\Users\Aimee\Documents\GitHub\rustStu\src\restaurant\target\debug\deps\librestaurant-5525da1842a10883.rlib: src\lib.rs
+
+C:\Users\Aimee\Documents\GitHub\rustStu\src\restaurant\target\debug\deps\restaurant-5525da1842a10883.d: src\lib.rs
+
+src\lib.rs:
---
+++ src/restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+C:\Users\Aimee\Documents\GitHub\rustStu\src\restaurant\target\debug\librestaurant.rlib: C:\Users\Aimee\Documents\GitHub\rustStu\src\restaurant\src\lib.rs
